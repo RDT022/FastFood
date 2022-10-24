fa6a4543c7f96661c0667108b1d4db07b04e98bf
ApLyDean <86749550+ApLyDean@users.noreply.github.com>
2022-10-23 23:56:21 -0400

    Trying to Resolve Conflict

---
+++ Assets/Scripts/Fading.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFadeIn : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup UIPhone;
+    [SerializeField] private CanvasGroup UIPlayButton;
+
+    public float timerValue = 1;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (timerValue > 0)
+        {
+            timerValue -= Time.deltaTime;
+        }
+        else 
+        {
+            timerValue = 0;
+        }
+        
+        if (timerValue == 0)
+        {
+            UIPhone.alpha += Time.deltaTime;
+            UIPlayButton.alpha += Time.deltaTime;
+        }
+    }
+    
+
+
+}
