34a1121f8bc1a8ad90b3a076d08ab7c3f6bcf11f
ApLyDean <86749550+ApLyDean@users.noreply.github.com>
2022-11-21 05:26:43 -0500

    star rating
    star rating shows up starting at 5 stars and should show the amount of lives the player has left. includes system for showing stars again if the player regains them

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -15,6 +15,22 @@ public class ScoreUI : MonoBehaviour
 
     public TextMeshProUGUI StarText;
 
+    //star rating ui//
+    public GameObject Star1;
+    public GameObject Star2;
+    public GameObject Star3;
+    public GameObject Star4;
+    public GameObject Star5;
+
+    void Start()
+    {
+        //star rating
+        Star1.SetActive(true);
+        Star2.SetActive(true);
+        Star3.SetActive(true);
+        Star4.SetActive(true);
+        Star5.SetActive(true);
+    }
 
     // Update is called once per frame
     void Update()
@@ -26,5 +42,33 @@ public class ScoreUI : MonoBehaviour
             StarText.SetText($"Star Rating: {player.Lives}");
         }
         ScoreText.SetText($"Score: {score}");
+
+        //yeah it's more than it needs to be but this way works too//
+        if (player.Lives == 5)
+        {
+            Star5.SetActive(true);
+        }
+        if (player.Lives == 4)
+        {
+            Star4.SetActive(true);
+            Star5.SetActive(false);
+        }        
+        if (player.Lives == 3)
+        {
+            Star3.SetActive(true);
+            Star4.SetActive(false);
+        }
+        if (player.Lives == 2)
+        {            
+            Star2.SetActive(true);
+            Star3.SetActive(false);
+        }
+        if (player.Lives == 1)
+        {
+            Star1.SetActive(true);
+            Star2.SetActive(false);
+        }
     }
+
+
 }
